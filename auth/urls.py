080e01406db6208df87bc0657cd3728908f22b3c
jitesh singh <jiteshjitsun@gmail.com>
2021-12-25 23:48:12 +0530

    new error is here set bloody object

--- auth/urls.py
+++ auth/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-   path('',views.home,name="auth"),
+   path('',views.home,name="home"),
    path('signup',views.signup,name="signup"),
    path('signin',views.signin,name="signin"),
    path('signout',views.signout,name="signout"),
