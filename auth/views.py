9f90456cfacfbdf546b3231e2526db586cda13f7
jitesh singh <jiteshjitsun@gmail.com>
2021-12-25 23:01:12 +0530

    merging signup with home

--- auth/views.py
+++ auth/views.py
@@ -13,6 +13,7 @@ from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes,force_str
 from django.core.mail import EmailMessage,send_mail
+from . tokens import generate_token
 
 def home(request): 
     return render(request,'auth/index.html')
