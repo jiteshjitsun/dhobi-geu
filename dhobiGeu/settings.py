20ee738e81a762d77795fd85ff99d148ca444e70
jitesh singh <jiteshjitsun@gmail.com>
2021-12-28 18:44:39 +0530

    logo added

--- dhobiGeu/settings.py
+++ dhobiGeu/settings.py
@@ -126,11 +126,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
