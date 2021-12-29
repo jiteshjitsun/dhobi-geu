bae8690c8fa0b226d9249d896ff6ae7670d280e2
jitesh singh <jiteshjitsun@gmail.com>
2021-12-29 16:09:12 +0530

    signup singin done

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/',views.signin,name='signin'),
+    path('signup/',views.signup, name='signup'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home/home.html')
+
+def signin(request):
+    return render(request, 'home/login.html')
+
+def signup(request):
+    return render(request, 'home/signup.html')
