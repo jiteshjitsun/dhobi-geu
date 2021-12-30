22e269e580c7dfc627dfc5bd4d46489da587b778
jitesh singh <jiteshjitsun@gmail.com>
2021-12-30 20:05:42 +0530

    wohooo

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('login/',views.signin,name='signin'),
     path('signup/',views.signup, name='signup'),
     path('contact/',views.contactForm, name='contact'),
-    path('userprofile/',views.userprofile, name='profile')
+    path('userprofile/',views.userprofile, name='profile'),
+    path('cart/',views.cart,name="cart"),
+    path('checkout/',views.checkout,name='checkout'),
+    path('update_item/', views.updateItem, name="update_item"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,99 @@
-from django.shortcuts import render
-from .models import Product
+from django.http.request import HttpRequest
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import JsonResponse
+import json
+import datetime
+from .models import * 
+from .utils import cookieCart, cartData, guestOrder
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.core.mail import send_mail, BadHeaderError
 # Create your views here.
 def home(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products':products,'cartItems':cartItems}
     return render(request,'home/home.html',context)
+
+def cart(request):
+	data = cartData(request)
+
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'home/cart.html', context)
+
+def checkout(request):
+	data = cartData(request)
+	
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'home/checkout.html', context)
+
+
+def updateItem(request):
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	else:
+		customer, order = guestOrder(request, data)
+
+	total = float(data['form']['total'])
+	order.transaction_id = transaction_id
+
+	if total == order.get_cart_total:
+		order.complete = True
+	order.save()
+
+	if order.shipping == True:
+		ShippingAddress.objects.create(
+		customer=customer,
+		order=order,
+		address=data['shipping']['address'],
+		city=data['shipping']['city'],
+		state=data['shipping']['state'],
+		zipcode=data['shipping']['zipcode'],
+		)
+
+	return JsonResponse('Payment submitted..', safe=False)
+
 
 def signin(request):
     return render(request, 'home/login.html')
